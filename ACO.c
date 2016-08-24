565af6db3883b1e8d626829c8ef3742930648f61
MutterPedro <pedro.mutter@gmail.com>
2016-08-23 23:12:08 -0300

    inserção do MPI

--- ACO.c
+++ ACO.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "omp.h"
+#include "mpi.h"
 #include <math.h>
 #define TEOR_EVAPORACAO 0.3
+#define Q 1
 
 int n;
-double qualidade[n][n],feromonio[n][n];
+double** qualidade;
+double** feromonio;
 
 typedef struct {
 	double x;
@@ -13,21 +16,120 @@ typedef struct {
 }Node;
 
 typedef struct {
-	Node* visitadas;
+	int* visitadas;
 	double caminho;
 }Ant;
 
-Ant formigas[n];
+Node* cidades;
+Ant formiga;
 
 double euclidiana(double x1, double y1,double x2, double y2){
 	return (sqrt(pow(x1-x2,2)+pow(y1-y2,2)));
 }
 
+double somatorioFeromonioQualidade();
+
+double somatorioDistanciaPercorrida();
+
+double probabilidade(int i,int j);
+
+double atualizarFeromonio(int i, int j, int somar);
+
+int jaVisitou(int formigaIdx, int cidadeIdx);
+
+int main(int argc, char** argv){
+	int size, rank, rc;
+	MPI_Status status;
+
+	MPI_Init(&argc,&argv); 
+	MPI_Comm_size (MPI_COMM_WORLD, &size); 
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	fflush(stdout);
+	
+	if(rank == 0){
+		scanf("%d",&n);
+		cidades = malloc(n*sizeof(Node));		
+
+		int i=0,j=0;
+		//inicializando matriz de distancias
+		for(i=0;i<n;i++){
+			scanf("%lf",&cidades[i].x);
+			scanf("%lf",&cidades[i].y);			
+		}
+
+		//inicializando matriz qualidade e feromonio
+		feromonio = malloc(n*sizeof(double*));
+		qualidade = malloc(n*sizeof(double*));
+		
+		for(i=0;i<n;i++){
+			feromonio[i] = malloc(n*sizeof(double));
+			qualidade[i] = malloc(n*sizeof(double));
+		}
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(i == j){
+					qualidade[i][j] = -1;
+					feromonio[i][j] = -1;
+				} else {
+					qualidade[i][j] = 1/euclidiana(cidades[i].x,cidades[j].x,cidades[i].y,cidades[j].y);
+					feromonio[i][j] = 1;			
+				}
+			}
+		}		
+	}
+	int proxima,i,j;
+	MPI_Bcast(&n,1, MPI_INT,0, MPI_COMM_WORLD);
+
+	if(rank > 0){
+		feromonio = malloc(n*sizeof(double*));
+		qualidade = malloc(n*sizeof(double*));
+		
+		for(i=0;i<n;i++){
+			feromonio[i] = malloc(n*sizeof(double));
+			qualidade[i] = malloc(n*sizeof(double));
+		}
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	for(i =0;i<n;i++){
+		for(j=0;j<n;j++){
+			MPI_Bcast(&qualidade[i][j],1, MPI_DOUBLE,0, MPI_COMM_WORLD);
+			MPI_Bcast(&feromonio[i][j],1, MPI_DOUBLE,0, MPI_COMM_WORLD);
+		}
+	}	
+
+	formiga.visitadas = malloc(n*sizeof(int));
+	formiga.visitadas[0] = rank;
+	double prob = 0;
+	for(i =1;i<n;i++){
+		for(j=0;j<n;j++){			
+			double p = probabilidade(rank,j);
+			if(p > prob && rank != j && jaVisitou(j) == 0){		
+				prob = p;
+				proxima = j;
+			}
+		}
+		formiga.visitadas[i] = proxima;
+	}
+	
+	printf("A formiga %d visitou: [",rank);
+	for(i =1;i<n;i++){
+		printf("%d ",formiga.visitadas[i]);
+	}
+	printf("]\n");	
+	
+	MPI_Finalize();
+	
+	
+	return 0;
+}
+
 double somatorioFeromonioQualidade(){
 	int i=0,j=0;
 	double soma = 0;
-	for(;i<n;){
-		for(;j<n;j++){
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
 			soma += feromonio[i][j]*qualidade[i][j];
 		}
 	}
@@ -38,8 +140,8 @@ double somatorioFeromonioQualidade(){
 double somatorioDistanciaPercorrida(){
 	int i=0;
 	double soma = 0;
-	for(;i<n;){
-		soma += 1/formigas[i].caminho;
+	for(i=0;i<n;i++){
+		//soma += Q/formigas[i].caminho;
 	}
 
 	return soma;
@@ -51,38 +153,19 @@ double probabilidade(int i,int j){
 	);
 }
 
-double atualizarFeromonio(int i, int j){
+double atualizarFeromonio(int i, int j, int somar){
 	return(
-		((1-TEOR_EVAPORACAO)*feromonio[i][j])+somatorioDistanciaPercorrida()
+		((1-TEOR_EVAPORACAO)*feromonio[i][j])+(somatorioDistanciaPercorrida()*somar)
 	);
 }
 
-int main(int argc, char** args){
-	scanf("%d",&n);
-	Node cidades[n];
-
-	int i=0,j=0,k=0,l=0;
+int jaVisitou(int cidadeIdx){
+	int i=0;
 	for(i=0;i<n;i++){
-		scanf("%lf",&cidades[i].x);
-		scanf("%lf",&cidades[i].y);
-		formigas[i].visitadas = malloc(n*sizeof(Node));
-		formigas[i].visitadas[0] = cidades[i];
-		formigas[i].caminho = 0;
-	}
-
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			if(i == j){
-				qualidade[i][j] = -1;
-				feromonio[i][j] = -1;
-			} else {
-				qualidade[i][j] = 1/euclidiana(cidades[i].x,cidades[j].x,cidades[i].y,cidades[j].y);
-				feromonio[i][j] = 0;			
-			}
+		if(formiga.visitadas[i] == cidadeIdx){
+			return 1;
 		}
-	}
-
-
+	}	
 	
 	return 0;
 }
